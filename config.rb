e0ce37dba5199cfad6a8f3f6ad4dd14c4239ec33
Stas Baydakov <sbaydakov@yahoo.com>
2013-04-13 14:14:55 -0700

    Compass Config & Styles
    - Modified Compass config for new directory structure
    - Style adjustment

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "skin/frontend/salsit/default/css"
+sass_dir = "skin/frontend/salsit/default/sass"
+images_dir = "skin/frontend/salsit/default/img"
+javascripts_dir = "skin/frontend/salsit/default/js"
+fonts_dir = "skin/frontend/salsit/default/fonts"
+
+output_style = :compact
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass skin/frontend/salsit/default/sass scss && rm -rf sass && mv scss sass
