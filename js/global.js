2e33fab963c0c81ee863fada04018d3db830bfa9
Stas Baydakov <sbaydakov@yahoo.com>
2013-04-16 00:39:35 -0700

    Hot Styles & Selectboxes
    - Add category class check for Hot Styles
    - Remove bg from selectboxes

--- js/global.js
+++ js/global.js
@@ -15,7 +15,7 @@
 		bind: function() {
 
 			// Replace select boxes with custom styling
-			//$j(".toolbar select").customSelect();
+			//$(".toolbar select").customSelect();
 				
 			// Keyboard navigation for slides
 			$(document).keydown(function(e){
@@ -29,7 +29,7 @@
 
 			var container = $('.product .gallery .more_images img');
 			container.on("mouseover", function() {
-				$(this).click(); console.log('click');
+				$(this).click();
 			});
 
 			
@@ -39,10 +39,12 @@
 
 			var radioContainer = $(".product .label-radio-configurable");
 
-			radioContainer.each( function(index) {
-				var radioTitle = $(this).find('span').text();
-				$(this).append('<div class="ss-radio">' + radioTitle + '</div>');
-			});
+			if (radioContainer.length) {
+				radioContainer.each( function(index) {
+					var radioTitle = $(this).find('span').text();
+					$(this).append('<div class="ss-radio">' + radioTitle + '</div>');
+				});
+			}
 
 			$(".ss-radio", radioContainer).on('click', function() {
 				$(this).parent().find('input:radio').attr('checked', 'checked');
@@ -137,7 +139,7 @@
 			scrollElement: 'html, body',
 			scrolling: false,
 			
-			init: function() { console.log('helpers init');
+			init: function() {
 				
 				this.toTop();
 				
@@ -193,7 +195,7 @@
 			},
 
 			toTop: function() {
-				console.log('totop initialized');
+
 				// Scroll to Top START
 			    $("#totop").hide();
 			    $(window).scroll(function () {
@@ -227,8 +229,6 @@
 
 	});
 
-})($j);
-
 
 /*!
  * jquery.customSelect() - v0.3.3
@@ -239,4 +239,7 @@
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
  * @license http://www.gnu.org/licenses/gpl.html GPL2 License
  */
-(function(a){a.fn.extend({customSelect:function(b){var c={customClass:null,mapClass:true,mapStyle:true},d=function(f,i){var e=f.find(":selected"),h=i.children(":first"),g=e.html()||"&nbsp;";h.html(g);setTimeout(function(){i.removeClass("customSelectOpen")},60)};if(typeof document.body.style.maxHeight==="undefined"){return this}b=a.extend(c,b);return this.each(function(){console.log(this);var e=a(this),g=a('<span class="customSelectInner" />'),f=a('<span class="customSelect" />');f.append(g);e.after(f);if(b.customClass){f.addClass(b.customClass)}if(b.mapClass){f.addClass(e.attr("class"))}if(b.mapStyle){f.attr("style",e.attr("style"))}e.addClass("hasCustomSelect").on("update",function(){d(e,f);var i=parseInt(e.outerWidth(),10)-(parseInt(f.outerWidth(),10)-parseInt(f.width(),10));f.css({display:"inline-block"});var h=f.outerHeight();if(e.attr("disabled")){f.addClass("customSelectDisabled")}else{f.removeClass("customSelectDisabled")}g.css({width:i,display:"inline-block"});e.css({"-webkit-appearance":"menulist-button",width:f.outerWidth(),position:"absolute",opacity:0,height:h,fontSize:f.css("font-size")})}).on("change",function(){f.addClass("customSelectChanged");d(e,f)}).on("keyup",function(){e.blur();e.focus()}).on("mousedown",function(){f.removeClass("customSelectChanged").addClass("customSelectOpen")}).focus(function(){f.removeClass("customSelectChanged").addClass("customSelectFocus")}).blur(function(){f.removeClass("customSelectFocus customSelectOpen")}).hover(function(){f.addClass("customSelectHover")},function(){f.removeClass("customSelectHover")}).trigger("update")})}})})(jQuery);
+(function(a){a.fn.extend({customSelect:function(b){var c={customClass:null,mapClass:true,mapStyle:true},d=function(f,i){var e=f.find(":selected"),h=i.children(":first"),g=e.html()||"&nbsp;";h.html(g);setTimeout(function(){i.removeClass("customSelectOpen")},60)};if(typeof document.body.style.maxHeight==="undefined"){return this}b=a.extend(c,b);return this.each(function(){var e=a(this),g=a('<span class="customSelectInner" />'),f=a('<span class="customSelect" />');f.append(g);e.after(f);if(b.customClass){f.addClass(b.customClass)}if(b.mapClass){f.addClass(e.attr("class"))}if(b.mapStyle){f.attr("style",e.attr("style"))}e.addClass("hasCustomSelect").on("update",function(){d(e,f);var i=parseInt(e.outerWidth(),10)-(parseInt(f.outerWidth(),10)-parseInt(f.width(),10));f.css({display:"inline-block"});var h=f.outerHeight();if(e.attr("disabled")){f.addClass("customSelectDisabled")}else{f.removeClass("customSelectDisabled")}g.css({width:i,display:"inline-block"});e.css({"-webkit-appearance":"menulist-button",width:f.outerWidth(),position:"absolute",opacity:0,height:h,fontSize:f.css("font-size")})}).on("change",function(){f.addClass("customSelectChanged");d(e,f)}).on("keyup",function(){e.blur();e.focus()}).on("mousedown",function(){f.removeClass("customSelectChanged").addClass("customSelectOpen")}).focus(function(){f.removeClass("customSelectChanged").addClass("customSelectFocus")}).blur(function(){f.removeClass("customSelectFocus customSelectOpen")}).hover(function(){f.addClass("customSelectHover")},function(){f.removeClass("customSelectHover")}).trigger("update")})}})})(jQuery);
+
+
+})($j);
